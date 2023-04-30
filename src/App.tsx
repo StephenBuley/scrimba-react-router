1a0fa41407f6fe66b22400b26cda8216308a3c81
Stephen Buley <stephen.a.buley@gmail.com>
2023-04-30 08:47:51 +0200

    Add css for header and footer
    Adds css for header and footer, as well as for full app width and
    viewport.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
 import './App.css'
 import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home'
-import { About } from './pages/About'
-import { Vans } from './pages/Vans'
+import { Home } from './Home'
+import { About } from './About'
+import { Vans } from './Vans'
 
 function App() {
   return (
     <BrowserRouter>
-      <header className="header">
-        <Link to="/" className="link--logo">
-          #VANLIFE
-        </Link>
-        <nav className="header--nav">
-          <Link to="/about" className="link--nav">
-            About
+      <div className="content">
+        <header className="header">
+          <Link to="/" className="link link--logo">
+            #VANLIFE
           </Link>
-          <Link to="/vans" className="link--nav">
-            Vans
-          </Link>
-        </nav>
-      </header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-      </Routes>
-      <footer className="footer">
-        <p className="footer--text">Ⓒ 2022 #VANLIFE</p>
-      </footer>
+          <nav className="header--nav">
+            <Link to="/about" className="link link--nav">
+              About
+            </Link>
+            <Link to="/vans" className="link link--nav">
+              Vans
+            </Link>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+        </Routes>
+        <footer className="footer">
+          <p className="footer--text">Ⓒ 2022 #VANLIFE</p>
+        </footer>
+      </div>
     </BrowserRouter>
   )
 }
